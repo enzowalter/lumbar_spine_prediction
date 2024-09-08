b70e69f975257eb791a7bc8470b994437355eb9b
enzo.walter <enzo.walter1994@gmail.com>
2024-09-09 00:19:00 +0200

    update sizes classifier

--- classification/launch_train.py
+++ classification/launch_train.py
@@ -1,24 +1,22 @@
 from train import train_model
 
 if __name__ == "__main__":
-    """
     m1 = train_model(
                 "../../REFAIT",
                 ["Spinal Canal Stenosis"],
                 "Sagittal T2/STIR",
                 "../trained_models/v0/model_slice_selection_st2.ts",
-                (96, 164),
-                (600, 600),
+                (128, 256),
+                (700, 700),
                 "model_classification_st2.pth",
             )
-    """
     m2 = train_model(
                 "../../REFAIT",
                 ["Right Neural Foraminal Narrowing"],
                 "Sagittal T1",
                 "../trained_models/v0/model_slice_selection_st1_right.ts",
-                (96, 164),
-                (600, 600),
+                (128, 256),
+                (700, 700),
                 "model_classification_st1_right.pth",
             )
     m3 = train_model(
@@ -26,8 +24,8 @@ if __name__ == "__main__":
                 ["Left Neural Foraminal Narrowing"],
                 "Sagittal T1",
                 "../trained_models/v0/model_slice_selection_st1_left.ts",
-                (96, 164),
-                (600, 600),
+                (128, 256),
+                (700, 700),
                 "model_classification_st1_left.pth",
             )
     m4 = train_model(
@@ -35,7 +33,7 @@ if __name__ == "__main__":
                 ["Right Subarticular Stenosis"],
                 "Axial T2",
                 "../trained_models/v0/model_slice_selection_axt2_right.ts",
-                (164, 164),
+                (224, 224),
                 (600, 600),
                 "model_classification_axt2_right.pth",
             )
@@ -44,12 +42,12 @@ if __name__ == "__main__":
                 ["Left Subarticular Stenosis"],
                 "Axial T2",
                 "../trained_models/v0/model_slice_selection_axt2_left.ts",
-                (164, 164),
+                (224, 224),
                 (600, 600),
                 "model_classification_axt2_left.pth",
             )
     
-    #print("Sagittal t2", m1)
+    print("Sagittal t2", m1)
     print("Sagittal t1 right", m2)
     print("Sagittal t2 left", m3)
     print("Sagittal ax2 right", m4)
