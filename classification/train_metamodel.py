f72a76373d3ab11e6f8b3622e049d36785f84c02
enzo.walter <enzo.walter1994@gmail.com>
2024-09-17 18:08:59 +0200

    update segmentation

---
+++ classification/train_metamodel.py
@@ -0,0 +1,318 @@
+import numpy as np
+import pandas as pd
+import tqdm
+import cv2
+import pydicom
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
+import torchvision.transforms.functional as FT
+import random
+import os
+
+import glob
+import torch
+import warnings
+warnings.filterwarnings("ignore") # warning on lstm
+
+from models import *
+
+############################################################
+############################################################
+#          GEN DATASET UTILS
+############################################################
+############################################################
+
+def get_instance(path):
+    return int(path.split("/")[-1].split('.')[0])
+
+def get_study_labels(study_id, df_study_labels, condition, levels, labels):
+    label_name = condition.lower().replace(" ", "_")
+    all_labels = df_study_labels[df_study_labels['study_id'] == study_id]
+    columns_of_interest = [col for col in all_labels.columns if label_name in col]
+    filtered_labels = all_labels[columns_of_interest].to_dict('records')[0]
+    final_labels = np.zeros(len(levels))
+
+    for level in levels:
+        level_name = level.lower().replace('/', '_')
+        for item in filtered_labels:
+            if level_name in item:
+                try:
+                    final_labels[levels[level]] = labels[filtered_labels[item]]
+                except Exception as e: # sometimes labels are NaN
+                    print("Error label", e)
+                    return None
+
+    return final_labels
+
+def extract_centered_square_with_padding(array, center_x, center_y, sizeX, sizeY):
+    square = np.zeros((sizeX, sizeY), dtype=array.dtype)
+    start_x = max(center_x - (sizeX // 2), 0)
+    end_x = min(center_x + (sizeX // 2), array.shape[0])
+    start_y = max(center_y - (sizeY // 2), 0)
+    end_y = min(center_y + (sizeY // 2), array.shape[1])
+    out_start_x = (sizeX // 2) - (center_x - start_x)
+    out_end_x = out_start_x + (end_x - start_x)
+    out_start_y = (sizeY // 2) - (center_y - start_y)
+    out_end_y = out_start_y + (end_y - start_y)
+    square[out_start_x:out_end_x, out_start_y:out_end_y] = array[start_x:end_x, start_y:end_y]
+    return square
+
+def clahe_equalization_norm2(image, clip_limit=2.0, grid_size=(8, 8)):
+    image = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
+    clahe = cv2.createCLAHE(clipLimit=clip_limit, tileGridSize=grid_size)
+    image = clahe.apply(np.uint8(image))
+    image = image.astype(np.float32) / 255.
+    return image
+
+def cut_crops(slices_path, x, y, crop_size, image_resize):
+    output_crops = np.zeros((len(slices_path), 128, 128))
+    for k, slice_path in enumerate(slices_path):
+        pixel_array = pydicom.dcmread(slice_path).pixel_array.astype(np.float32)
+        pixel_array = cv2.resize(pixel_array, image_resize, interpolation=cv2.INTER_LINEAR)
+        crop = extract_centered_square_with_padding(pixel_array, y, x, *crop_size) # x y reversed in array
+        crop = clahe_equalization_norm2(crop)
+        crop = cv2.resize(crop, (128, 128), interpolation=cv2.INTER_LINEAR)
+        output_crops[k, ...] = crop
+    return output_crops
+
+############################################################
+############################################################
+#          GEN DATASET
+############################################################
+############################################################
+
+def generate_dataset(input_dir, crop_description, crop_condition, label_condition, crop_size, image_resize):
+    LEVELS = {"L1/L2":0, "L2/L3":1, "L3/L4":2, "L4/L5":3, "L5/S1":4}
+    LABELS = {"Normal/Mild" : 0, "Moderate": 1, "Severe": 2}
+
+    df_study_labels = pd.read_csv(f"{input_dir}/train.csv")
+    df_study_coordinates = pd.read_csv(f"{input_dir}/train_label_coordinates.csv")
+    df_study_descriptions = pd.read_csv(f"{input_dir}/train_series_descriptions.csv")
+    studies_id = df_study_labels["study_id"].to_list()
+
+    dataset = list()
+    for study_id in tqdm.tqdm(studies_id, desc="Generates classification dataset"):
+
+        series_id = df_study_descriptions[(df_study_descriptions['study_id'] == study_id)
+                                        & (df_study_descriptions['series_description'] == crop_description)]['series_id'].to_list()
+        
+        for s_id in series_id:
+            coordinates_dict = df_study_coordinates[(df_study_coordinates['study_id'] == study_id)
+                                & (df_study_coordinates['condition'] == crop_condition)
+                                & (df_study_coordinates['series_id'] == s_id)].to_dict('records')
+
+            # add to dataset only if all vertebraes in gt
+            if len(coordinates_dict) == len(LEVELS):
+                all_slices_path = sorted(glob.glob(f"{input_dir}/train_images/{study_id}/{s_id}/*.dcm"), key = lambda x : get_instance(x))
+                gt_labels = get_study_labels(study_id, df_study_labels, label_condition, LEVELS, LABELS)
+
+                if gt_labels is not None:
+                    for coordinate in coordinates_dict:
+                        try:
+                            dataset_item = dict()
+
+                            # get original slices
+                            dataset_item['slices_path'] = list()
+                            for idx, sp in enumerate(all_slices_path):
+                                if get_instance(sp) == coordinate['instance_number']:
+                                    if idx == 0:
+                                        dataset_item['slices_path'].append(all_slices_path[idx])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx + 1])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx + 2])
+                                    elif idx == len(all_slices_path) - 1:
+                                        dataset_item['slices_path'].append(all_slices_path[idx - 2])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx - 1])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx])
+                                    else:
+                                        dataset_item['slices_path'].append(all_slices_path[idx - 1])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx])        
+                                        dataset_item['slices_path'].append(all_slices_path[idx + 1])
+
+                                    break
+
+                            idx_level = LEVELS[coordinate['level']]
+                            x, y = coordinate['x'], coordinate['y']
+                            original_shape = pydicom.dcmread(f"{input_dir}/train_images/{study_id}/{s_id}/{coordinate['instance_number']}.dcm").pixel_array.shape
+                            x = int((x / original_shape[1]) * image_resize[1]) 
+                            y = int((y / original_shape[0]) * image_resize[0])
+
+                            dataset_item['study_id'] = study_id
+                            dataset_item['series_id'] = s_id
+                            dataset_item['position'] = (x, y)
+                            dataset_item['gt_label'] = gt_labels[idx_level]
+                            dataset_item['crop_size'] = crop_size
+                            dataset_item['image_resize'] = image_resize
+
+                            dataset.append(dataset_item)
+
+                        except Exception as e:
+                            print("Error add item", e)
+                            continue
+    return dataset
+
+
+############################################################
+############################################################
+#          DATALOADER
+############################################################
+############################################################
+
+def tensor_augmentations(tensor_image):
+    angle = random.uniform(-10, 10)
+    tensor_image = FT.rotate(tensor_image, angle)
+    noise = torch.randn(tensor_image.size()) * 0.01
+    tensor_image = tensor_image + noise
+    return tensor_image
+
+class CropClassifierDataset(Dataset):
+    def __init__(self, infos, is_train=False):
+        self.datas = infos
+        self.is_train = is_train
+
+    def __len__(self):
+        return len(self.datas)
+
+    def __getitem__(self, idx):
+        data = self.datas[idx]
+        slices_to_use = data['slices_path']
+        x, y = data['position']
+        crops = cut_crops(slices_to_use, x, y, data['crop_size'], data['image_resize'])
+        crops = torch.tensor(crops).float()
+        if self.is_train:
+            crops = tensor_augmentations(crops)
+        return crops, data['gt_label']
+
+############################################################
+############################################################
+#          TRAINING
+############################################################
+############################################################
+
+def calculate_log_loss(predictions, labels, weights, epsilon=1e-9):
+    probabilities = torch.clamp(F.softmax(predictions, dim=1), min=epsilon, max=1 - epsilon)
+    one_hot_labels = F.one_hot(labels, num_classes=3).float()
+    log_loss_batch = -torch.sum(one_hot_labels * torch.log(probabilities), dim=1)
+    weights_per_sample = weights[labels.cpu().numpy()]
+    weighted_log_loss_batch = log_loss_batch * torch.tensor(weights_per_sample, device=labels.device)
+    return weighted_log_loss_batch
+
+def validate(model_features, metamodel, loader, criterion, device):
+    metamodel.eval()
+    model_features.eval()
+
+    classification_loss_sum = 0
+    total_log_loss = 0
+    total_samples = 0
+    weights = np.array([1, 2, 4])
+    matrix = np.zeros((3, 3))
+    with torch.no_grad():
+        for images, labels_gt in tqdm.tqdm(loader, desc="Valid"):
+            labels_gt = labels_gt.to(device).long()
+
+            features = model_features.forward_encoders(images.to(device))
+            predictions = metamodel(features)
+            
+            i = torch.argmax(predictions, dim = 1)
+            matrix[labels_gt.item(), i.item()] += 1
+
+            loss = criterion(predictions, labels_gt)
+            classification_loss_sum += loss.item()
+
+            log_loss_batch = calculate_log_loss(predictions, labels_gt, weights)
+            total_log_loss += log_loss_batch.sum().item()
+            total_samples += labels_gt.size(0)
+
+        avg_classification_loss = classification_loss_sum / len(loader)
+        avg_log_loss = total_log_loss / total_samples
+
+    avg_classification_loss = classification_loss_sum / len(loader)
+
+    weights = np.array([1, 2, 4])
+    TP = np.diag(matrix)
+    FP = np.sum(matrix, axis=0) - TP
+    FN = np.sum(matrix, axis=1) - TP
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * (precision * recall) / (precision + recall)
+    weighted_precision = np.sum(weights * precision) / np.sum(weights)
+    weighted_recall = np.sum(weights * recall) / np.sum(weights)
+    weighted_f1_score = np.sum(weights * f1_score) / np.sum(weights)
+
+    print(matrix)
+    print("Precision:", weighted_precision, "Recall:", weighted_recall, "F1:", weighted_f1_score)
+    return {"loss": avg_classification_loss, "logloss": avg_log_loss}
+
+def train_epoch(model_features, metamodel, loader, criterion, optimizer, device):
+    metamodel.train()
+    epoch_loss = 0
+    for images, labels in tqdm.tqdm(loader, desc="Training", total=len(loader)):
+        optimizer.zero_grad()
+        images = images.to(device)
+        labels = labels.to(device).long()
+
+        with torch.no_grad():
+            features = model_features.forward_encoders(images.to(device))
+        predictions = metamodel(features)
+
+        loss = criterion(predictions, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item() / len(loader)
+
+    return epoch_loss
+
+
+def train_metamodel(input_dir, model_name, crop_description, crop_condition, label_condition, crop_size, image_resize, pretrained_path):
+
+    # get dataset
+    dataset = generate_dataset(input_dir, crop_description, crop_condition, label_condition, crop_size, image_resize)
+    nb_valid = int(len(dataset) * 0.1)
+    train_dataset = CropClassifierDataset(dataset[nb_valid:], is_train=True)
+    valid_dataset = CropClassifierDataset(dataset[:nb_valid], is_train=False)
+    train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=4)
+    valid_loader = DataLoader(valid_dataset, batch_size=1)
+
+    # get model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    feature_model = FoldModelClassifier(
+        n_classes=3,
+        n_fold_classifier=3,
+        backbones=['densenet201.tv_in1k', 'seresnext101_32x4d.gluon_in1k', 'convnext_base.fb_in22k_ft_in1k', 'dm_nfnet_f0.dm_in1k', 'mobilenetv3_small_100.lamb_in1k'],
+        #backbones=['densenet201.tv_in1k', 'seresnext101_32x4d.gluon_in1k', 'convnext_base.fb_in22k_ft_in1k', 'dm_nfnet_f0.dm_in1k', 'mobilenetv3_small_100.lamb_in1k', 'vit_small_patch16_224.augreg_in21k_ft_in1k'],
+        features_size=256,
+    )
+    feature_model.load_state_dict(torch.load(pretrained_path))
+    feature_model = feature_model.to(device)
+    metamodel = MetaModelClassifier(feature_size=256, seq_lenght=5, n_classes=3)
+    metamodel = metamodel.to(device)
+
+    # train with folding
+    optimizer = torch.optim.AdamW(metamodel.parameters(), lr=0.001)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=20, gamma=0.5)
+    criterion = torch.nn.CrossEntropyLoss(weight = torch.tensor([1, 2, 4]).float().to(device))
+    best = 123456
+    for epoch in range(10):
+        loss_train = train_epoch(feature_model, metamodel, train_loader, criterion, optimizer, device)
+        metrics = validate(feature_model, metamodel, valid_loader, criterion, device)
+        print("Epoch", epoch, "train_loss=", loss_train, "metrics=", metrics)
+        if metrics['loss'] < best:
+            print("New best model !", model_name)
+            best = metrics["loss"]
+            torch.save(metamodel.state_dict(), model_name)
+        scheduler.step()
+        print('-' * 50)
+    return best
+
+if __name__ == "__main__":
+    best_logloss1 = train_metamodel(
+                    input_dir="../../REFAIT",
+                    model_name="classification_spinal_canal_stenosis_metamodel.pth",
+                    crop_condition="Spinal Canal Stenosis",
+                    label_condition="Spinal Canal Stenosis",
+                    crop_description="Sagittal T2/STIR",
+                    crop_size=(64, 96),
+                    image_resize=(640, 640),
+                    pretrained_path="classification_spinal_canal_stenosis.pth"
+    )
+    print(best_logloss1)
